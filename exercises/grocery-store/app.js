005dfecca71e0408542fd156cac2dc28deae92c5
Nick Philips <nicholasphilips622@gmail.com>
2019-01-24 17:56:48 -0700

    adding grocery-store

---
+++ exercises/grocery-store/app.js
@@ -0,0 +1,16 @@
+var shopper = {
+    name: "Billy Bob", 
+    age: 92, 
+    gotMilk: true, 
+    method: function(){
+        return "I'm a pretty butterfly";
+    }, 
+    cart: ["Milk", "Butter", "Cheese"]};
+
+
+console.log(shopper.name);
+console.log(shopper.age);
+console.log(shopper.gotMilk);
+console.log(shopper.cart);
+console.log(shopper.method());
+
