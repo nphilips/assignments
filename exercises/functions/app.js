c1537db4557e630bbe2acc06d9a81b3af7396e2b
Nick Philips <nicholasphilips622@gmail.com>
2019-01-28 13:50:13 -0700

    git update

---
+++ exercises/functions/app.js
@@ -0,0 +1,30 @@
+// myfunc()
+
+// function myFunc(){
+//     console.log("This is a function declaration")
+// }
+
+// var myOtherFunc = function(){
+//     console.log("This is a function expression")
+// }
+
+// myOtherFunc()
+
+// function sum(a, b){
+//     return a + b
+// }
+
+// var result = sum(5,10)
+
+// function largest(a, b, c){
+//     console.log(largest())
+// }
+// console.log(largest.max.apply(10, 25, 4))
+
+function myFunc(){
+    if(true){
+        var a = "Hello"
+    }
+    console.log(a)
+}
+myFunc()
---
+++ projects/social-media/social.js
@@ -0,0 +1,6 @@
+var personOne = {
+    name: "Bob",
+    age: 31,
+    hobbies: ["Reading", "hiking", "biking"],
+    friends: ["Sarah", "Samuel", "Jakobe"]
+}
