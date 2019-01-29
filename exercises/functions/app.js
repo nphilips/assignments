a3857194322eb11bad26cd490f7870929932277c
Nick Philips <nicholasphilips622@gmail.com>
2019-01-29 10:10:09 -0700

    git update

--- exercises/functions/app.js
+++ exercises/functions/app.js
@@ -1,3 +1,4 @@
+{/* first eddition
 // function sum(a, b){
 //     return a + b
 // }
@@ -51,12 +52,45 @@
 
 // function stringSplit(a){
 //     if (a.length<=20){
-//         return a.split(" ")
+//         return a.split()
 //     } else if(a.length>20){
 //         var halfString = a.length / 2
-//         return a.splice("")
+//         return a.splice
 //     } else{
 //         return "Broken"
 //     }
 // }
 // console.log(stringSplit("I am a beautiful butterfly"))
+*/}
+
+{/* Mini Quiz
+////     1. Write a function that takes a string as an parameter/argument and returns
+////     that string uppercased.
+
+// function upperCaseMe(str){
+//     return str.toUpperCase()
+// }
+// console.log(upperCaseMe("good morning!")) // GOOD MORNING!
+
+
+////    2. Write a function that takes a string and returns that string reversed.
+
+// function reverseMe(str){
+//     return str.split("").reverse().join("")
+// }
+
+// console.log(reverseMe("What do I spell backwards?")) // ?sdrawkcab lleps I od tahW
+
+
+
+////     3. Write a function that takes a string and a number.
+////     Return the string slice from index 0 to that argument number.
+
+// function sliceMe(str, num){
+//     return str.slice(0,num)
+// }
+
+// console.log(sliceMe("This is the string to slice.", 10)) // This is th
+*/}
+
+
--- exercises/string-method/app.js
+++ exercises/string-method/app.js
@@ -24,11 +24,35 @@
 // console.log(sliceThisString.slice(3))
 
 
-var strHello = "hElLo "
-var helloWorld = "Hello World"
-console.log(strHello + strHello.toUpperCase() + strHello.toLowerCase())
+// var strHello = "hElLo "
+// var helloWorld = "Hello World"
+// //console.log(strHello + strHello.toUpperCase() + strHello.toLowerCase())
 
-console.log(Math.floor(234/5))
-var delWold= Math.floor(helloWorld.length / 2)
-console.log(strHello.slice())
-console.log(helloWorld.slice(delWorld.length - delWorld, delWrold.length))
+// //console.log(Math.floor(234/5))
+// var delLett = strHello.length / 2
+// var delWorld = helloWorld.length / 2
+
+// strHello.slice(strHello.length- delLett, strHello.length)
+// helloWorld.slice(helloWorld.length - delWorld, helloWorld.length)
+// console.log(strHello)
+// console.log(helloWorld)
+
+// function capilizeAndLowercase(str){
+//     var middle = Math.floor(str.length / 2)
+//     var str1= str.slice(0,middle).toUpperCase()
+//     var str2= str.slice(middle).toLowerCase()
+//     return str1+ str2
+// }
+
+// console.log(capilizeAndLowercase("Hello")) // => "HEllo"
+// console.log(capilizeAndLowercase("Hello World")) // => "HELLO world"
+
+function capitalize(str){
+    var splitWords = str.split(" ")
+    for(var i=0; i<splitWords.length; i++){
+        splitWords[i] = splitwords[i][0].toUpperCase() + splitWords[i].slice(1)
+    }
+    return splitWords.join(" ")
+}
+
+console.log(capitalize("hey friends! practice practice practice"))
