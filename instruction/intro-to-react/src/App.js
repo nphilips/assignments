44f406f98fa07315ac209d70a7042063a7b20cef
Nick Philips <nicholasphilips622@gmail.com>
2019-02-19 11:28:04 -0700

    first react

---
+++ instruction/intro-to-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from './Nav.js'
+import './style.css'
+
+function App(){
+    return (
+    <div>
+        <h1 className='hello-world'>Hello World</h1>
+        <Nav />
+    </div>
+    )
+}
+
+export default App
---
+++ instruction/intro-to-react/src/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav(){
+    return (
+        <nav>
+            <h3>I'm in the Nav.js file</h3>
+        </nav>
+    )
+}
+
+export default Nav
