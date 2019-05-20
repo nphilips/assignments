f37b8805e8a756f7ddfc70bc19210f7da371c81f
nphilips <nicholasphilips622@gmail.com>
2019-05-19 22:06:26 -0600

    zen-garden update

--- projects/match-your-malts-v2/src/index.js
+++ projects/match-your-malts-v2/src/index.js
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.js'
 import BeerProvider from './context/BeerProvider.js'
+import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.render(
-    <BeerProvider>
-        <App />
-    </BeerProvider>, 
+    <BrowserRouter>
+        <BeerProvider>
+            <App />
+        </BeerProvider>
+    </BrowserRouter>, 
 document.getElementById('root'))
